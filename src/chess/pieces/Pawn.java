011e9d81bcff05bf6c6bc0389da500c03373ddd4
Radosław Podgórski <rpod002@gmail.com>
2025-05-09 20:28:33 +0200

    Added Pawn promotion to the game logic

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -17,23 +17,39 @@ public class Pawn extends Piece {
         int deltaX = move.getTo().getX() - move.getFrom().getX();
         int deltaY = move.getTo().getY() - move.getFrom().getY();
 
-        Piece targetPiece = board.getPieceAtPosition(move.getTo().getX(), move.getTo().getY());
-
-        if (deltaX == direction && deltaY == 0 && targetPiece == null) {
+        // ruch o jedno pole do przodu
+        if (deltaX == direction && deltaY == 0 &&
+                board.getPieceAtPosition(move.getTo().getX(), move.getTo().getY()) == null) {
             return true;
         }
 
-        if (move.getFrom().getX() == startRow && deltaX == 2 * direction && deltaY == 0) {
-            Position middlePosition = new Position(move.getFrom().getX() + direction, move.getFrom().getY());
-            if (targetPiece == null && board.getPieceAtPosition(middlePosition.getX(), middlePosition.getY()) == null) {
-                return true;
-            }
+        // ruch o dwa pola z pozycji początkowej
+        if (move.getFrom().getX() == startRow && deltaX == 2 * direction && deltaY == 0 &&
+                board.getPieceAtPosition(move.getTo().getX(), move.getTo().getY()) == null &&
+                board.getPieceAtPosition(move.getFrom().getX() + direction, move.getFrom().getY()) == null) {
+            return true;
         }
 
-        if (deltaX == direction && Math.abs(deltaY) == 1 && targetPiece != null && targetPiece.getColor() != color) {
-            return true;
+        // bicie
+        if (deltaX == direction && Math.abs(deltaY) == 1) {
+            Piece target = board.getPieceAtPosition(move.getTo().getX(), move.getTo().getY());
+            return target != null && target.getColor() != color;
         }
 
         return false;
+    }
+
+    @Override
+    public void setPosition(Position position) {
+        super.setPosition(position);
+
+        boolean reachedEnd = (color == Color.WHITE && position.getX() == 0) ||
+                (color == Color.BLACK && position.getX() == 7);
+
+        if (reachedEnd) {
+            Queen promotedQueen = new Queen(color, position);
+            promotedQueen.setBoard(board); // 🔥 KONIECZNE!
+            board.setPieceAtPosition(position.getX(), position.getY(), promotedQueen);
+        }
     }
 }
